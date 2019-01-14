2a3badcea6f793f5f5416a4f871bb5e9b6005733
Thierry Parent <tparent@absolunet.com>
2019-01-14 09:00:26 -0500

    Initial invoice fake API

--- webpack.mix.js
+++ webpack.mix.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 
 mix.setPublicPath('public')
 	.js('src/index.js', 'public/build/app.js')
+	.copyDirectory('src/api', 'public/api')
 	.then(() => {
 		const manifestPath = path.resolve(__dirname, 'public', 'mix-manifest.json');
 		if (fs.existsSync(manifestPath)) {
